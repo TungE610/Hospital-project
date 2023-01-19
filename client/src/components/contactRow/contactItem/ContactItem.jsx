69634d2d647fe279da03bdd65aea4982ee15c486
TungE610 <81414026+TungE610@users.noreply.github.com>
2023-01-20 00:44:12 +0700

    reformat code ContactItem component

--- client/src/components/contactRow/contactItem/ContactItem.jsx
+++ client/src/components/contactRow/contactItem/ContactItem.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import styles from './style.module.css'
+import styles from './style.module.css';
 
 const ContactItem = (props) => {
 
-
 	return (
 		<div className= {styles.contactItem}>
-				<img src={require(`../../../assets/${props.iconLink}`)} className={styles.contactItemLogo}/>
-				<div className={styles.itemInfo}>
-						{props.itemInfo}
-				</div>
+			<img src={require(`../../../assets/${props.iconLink}`)} className={styles.contactItemLogo} />
+			<div className={styles.itemInfo}>
+				{props.itemInfo}
+			</div>
 		</div>
 	)
-
 }
 
 export default ContactItem;
