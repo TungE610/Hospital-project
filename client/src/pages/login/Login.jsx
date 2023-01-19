3b1f68dd474a5f873b4da7aef7e630b793ba75fe
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2023-01-20 01:31:44 +0700

    reformat Login Page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import styles from './Login.module.css'
+import styles from './Login.module.css';
 import { AccountBox } from "../../components/accountBox";
 
 const Login = () => {
 
-	return (
-		<div className={styles.loginPage}>
-				<AccountBox/>
-		</div>
-	)
+  return (
+    <div className={styles.loginPage}>
+        <AccountBox/>
+    </div>
+  )
 
 }
 
