ca33793f844e37730b9de6f5b45a1d55b929b1e1
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2023-01-20 01:02:13 +0700

    reformat LoginForm

--- client/src/components/accountBox/loginForm.jsx
+++ client/src/components/accountBox/loginForm.jsx
@@ -1,5 +1,10 @@
 import React, { useContext, useState } from "react";
-import axios from 'axios'
+import axios from 'axios';
+import { Marginer } from "../marginer";
+import { AccountContext } from "./AccountContext";
+import { notification } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import LogginContext from "./LogginContext"; 
 import {
   BoldLink,
   BoxContainer,
@@ -8,124 +13,122 @@ import {
   MutedLink,
   SubmitButton,
 } from "./common";
-import { Marginer } from "../marginer";
-import { AccountContext } from "./AccountContext";
-import { notification } from "antd";
-import { Link, useNavigate } from "react-router-dom";
-import LogginContext from "./LogginContext"; 
 
 export function LoginForm() {
 
-  	const {switchToSignup} = useContext(AccountContext);
-  	const [loginData, setLoginData] = useState({});
-	const [ ,setLoggedIn] = useContext(LogginContext);
-	const baseUrl = 'https://hospital-project-api.onrender.com/api';
+  const {switchToSignup} = useContext(AccountContext);
+  const [loginData, setLoginData] = useState({});
+  const [ ,setLoggedIn] = useContext(LogginContext);
+  const baseUrl = 'https://hospital-project-api.onrender.com/api';
 
-  	let navigate = useNavigate();
+  let navigate = useNavigate();
 
-	const loginHandler = async () => {
-		 try {
-			axios(`${baseUrl}/users/login`).then(response => {
+  const loginHandler = async () => {
+     try {
+      axios(`${baseUrl}/users/login`).then(response => {
 
-			});
+      });
 
-			 const body = {
-				email : loginData.email,
-				password : loginData.password
-			};
+       const body = {
+        email : loginData.email,
+        password : loginData.password
+      };
 
-			axios.post(`${baseUrl}/users/login`, body).then(response => {
+      axios.post(`${baseUrl}/users/login`, body).then(response => {
 
-				if(response.status === 200) {
-					successNotification()
-					navigate('/TopPage')
-					const user =  response.data
-					setLoggedIn({isLoggedIn : true, role : user.role, doctor_id : user.doctor_id, room_id : user.room_id});
+        if(response.status === 200) {
 
-					sessionStorage.clear();
-					sessionStorage.setItem("email", user.email);
-					sessionStorage.setItem("role", user.role);
-					sessionStorage.setItem("doctor_id", user.doctor_id);
-					sessionStorage.setItem("room_id", user.room_id);
-				} else {
+          successNotification();
 
-				}
-			})
-			.catch(error => {
-				loginFailNotification();
-				navigate('/Login');
-			}
-			);
-		}catch(error) {
-			alert("Please wait some second...")
-		}
-		
-	}
-	const successNotification = () => {
+          navigate('/TopPage');
 
-		notification["success"]({
-			message: 'SUCCESSFULL',
-			description:
-				`You have just successfully login !!`,
-		});
-	};
+          const user =  response.data;
+
+          setLoggedIn({isLoggedIn : true, role : user.role, doctor_id : user.doctor_id, room_id : user.room_id});
+
+          sessionStorage.clear();
+          sessionStorage.setItem("email", user.email);
+          sessionStorage.setItem("role", user.role);
+          sessionStorage.setItem("doctor_id", user.doctor_id);
+          sessionStorage.setItem("room_id", user.room_id);
+
+        } else {
+
+        }
+      })
+      .catch(error => {
+        loginFailNotification();
+        navigate('/Login');
+      }
+      );
+    }catch(error) {
+      alert("Please wait some second...")
+    }
+    
+  }
+  const successNotification = () => {
+
+    notification["success"]({
+      message: 'SUCCESSFULL',
+      description:`You have just successfully login !!`,
+    });
+  };
   
-	const loginFailNotification = () => {
+  const loginFailNotification = () => {
 
-		notification["error"]({
-			message: 'UNSUCCESSFUL',
-			description:
-				'Your email or password is incorrect. Please try again !!',
-		});
-	};
+    notification["error"]({
+      message: 'UNSUCCESSFUL',
+      description:'Your email or password is incorrect. Please try again !!',
+    });
+  };
 
-	const showNotiBoxHandler = () => {
+  const showNotiBoxHandler = () => {
 
-		localStorage.setItem('showNotiBox', 'true');
-	}
+    localStorage.setItem('showNotiBox', 'true');
+  }
 
-	return (
-    	<BoxContainer>
-      		<FormContainer>
-        	
-				<Input 
-					type="email" 
-					placeholder="Email" 			
-					onChange={(e) => {
-						setLoginData( prev => { return {...prev, email: e.target.value }})
-					}}
-				/>
-        	
-				<Input 
-					type="password" 
-					placeholder="Password" 
-					onChange={(e) => {
-						setLoginData( prev => { return {...prev, password: e.target.value }})
-					}}
-				/>
+  return (
+      <BoxContainer>
+        <FormContainer>
+          
+          <Input 
+            type="email" 
+            placeholder="Email" 			
+            onChange={(e) => {
+              setLoginData( prev => { return {...prev, email: e.target.value }});
+            }}
+          />
+            
+          <Input 
+            type="password" 
+            placeholder="Password" 
+            onChange={(e) => {
+              setLoginData( prev => { return {...prev, password: e.target.value }});
+            }}
+          />
       
-	  		</FormContainer>
+        </FormContainer>
 
-    	<Marginer direction="vertical" margin={10} />
+      <Marginer direction="vertical" margin={10} />
 
-      	<MutedLink href="#"> Forget your password ?</MutedLink>
+      <MutedLink href="#"> Forget your password ?</MutedLink>
 
-      	<Marginer direction="vertical" margin="1.6em" />
+      <Marginer direction="vertical" margin="1.6em" />
 
-      	<SubmitButton type="submit" onClick={() => {loginHandler()}}> SIGN IN AS DOCTOR </SubmitButton>
-	  
-	  	<SubmitButton>
-			<Link to="/TopPage" onClick={showNotiBoxHandler}>
-				USE AS GUEST
-				</Link>
-			</SubmitButton>
-      	<Marginer direction="vertical" margin="1em" />
-      	<MutedLink href="#">
-        	Don't have an accoun?{" "}
+      <SubmitButton type="submit" onClick={() => {loginHandler()}}> SIGN IN AS DOCTOR </SubmitButton>
+    
+      <SubmitButton>
+      <Link to="/TopPage" onClick={showNotiBoxHandler}>
+        USE AS GUEST
+        </Link>
+      </SubmitButton>
+        <Marginer direction="vertical" margin="1em" />
+        <MutedLink href="#">
+          Don't have an accoun?{" "}
         <BoldLink href="#" onClick={switchToSignup}>
           Signup
         </BoldLink>
-		
+    
       </MutedLink>
     </BoxContainer>
   );
