6d5901fee86c35320f104246103c3f032c390742
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2023-01-20 01:31:17 +0700

    reformat topPage

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import {Outlet} from "react-router-dom";
-import styles from './Layout.module.css'
-import Login from './login/Login'
+import styles from './Layout.module.css';
+import Login from './login/Login';
 
 const Layout = () => {
+  
   return (
     <div className={styles.layout}>
       <Outlet />
