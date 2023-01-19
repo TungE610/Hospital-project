ea07f9374707e8cc70a0995d075450d18e094db4
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2023-01-20 01:28:32 +0700

    reformat ContactItem

--- client/src/components/contactRow/contactItem/ContactItem.jsx
+++ client/src/components/contactRow/contactItem/ContactItem.jsx
@@ -3,14 +3,14 @@ import styles from './style.module.css';
 
 const ContactItem = (props) => {
 
-	return (
-		<div className= {styles.contactItem}>
-			<img src={require(`../../../assets/${props.iconLink}`)} className={styles.contactItemLogo} />
-			<div className={styles.itemInfo}>
-				{props.itemInfo}
-			</div>
-		</div>
-	)
+  return (
+    <div className= {styles.contactItem}>
+      <img src={require(`../../../assets/${props.iconLink}`)} className={styles.contactItemLogo} />
+      <div className={styles.itemInfo}>
+        {props.itemInfo}
+      </div>
+    </div>
+  )
 }
 
 export default ContactItem;
