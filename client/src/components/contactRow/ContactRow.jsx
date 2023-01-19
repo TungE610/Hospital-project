ff5fae5816fb456d22bd87ca7882599b4805c6e7
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2023-01-20 01:27:54 +0700

    reformat contactRow

--- client/src/components/contactRow/ContactRow.jsx
+++ client/src/components/contactRow/ContactRow.jsx
@@ -4,14 +4,14 @@ import ContactItem from "./contactItem/ContactItem";
 
 const ContactRow = () => {
   
-	return (
-		<div className={styles.ContactRow}>
-			<ContactItem iconLink="placeholder.png" itemInfo="Số 1, Đại Cồ Việt, HBT"/>
-			<ContactItem iconLink="phone-call.png" itemInfo="099999999"/>
-			<ContactItem iconLink="gmail.png" itemInfo="Tung2082002@gmail.com"/>
-			<ContactItem iconLink="clock.png" itemInfo="Mon-Fri: 7am - 5pm"/>
-		</div>
-	)
+  return (
+    <div className={styles.ContactRow}>
+      <ContactItem iconLink="placeholder.png" itemInfo="Số 1, Đại Cồ Việt, HBT"/>
+      <ContactItem iconLink="phone-call.png" itemInfo="099999999"/>
+      <ContactItem iconLink="gmail.png" itemInfo="Tung2082002@gmail.com"/>
+      <ContactItem iconLink="clock.png" itemInfo="Mon-Fri: 7am - 5pm"/>
+    </div>
+  )
 }
 
 export default ContactRow;
